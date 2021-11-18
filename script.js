5f6820e37d761c9144db253a077dc7d599e28c4b
Stephanie <sandora375@outlook.com>
2021-11-18 03:29:18 -0600

    added rows and need to fix width

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ console.log(today);
 //THEN I am presented with timeblocks for standard business hours
 
 
+
 //WHEN I view the timeblocks for that day
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
