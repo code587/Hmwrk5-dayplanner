53739e2d234951b4b84633121a94c6f1a6de610d
Stephanie <sandora375@outlook.com>
2021-11-19 01:26:44 -0600

    attempted local storage saving to update later

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 //You'll need to use the [Moment.js](https://momentjs.com/) 
+let hourlyTask = document.querySelector("#activity");
+let saveActivity = document.querySelector(".saveBtn");
+let task = localStorage.getItem("task");
+
+renderLastRegistered();
+
+console.log(saveActivity);
 
 let today = moment()
 $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
 console.log(today);
 
-
 //WHEN I view the timeblocks for that day
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-let hourlyTask = document.querySelector("#activity");
 
 hourlyTask.addEventListener("click", function() {
   hourlyTask.textContent = ""
 })
-console.log(task);
+console.log(hourlyTask);
 
+function renderLastRegistered() {
+    let activity = localStorage.getItem("activity");
+
+    if (activity) {
+        return;
+    }
+}
+saveActivity.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+let hourlyTask = document.querySelector("#activity").value;
+
+localStorage.setItem("hourlyTask", hourlyTask);
+renderLastRegistered();
+})
+console.log(localStorage);
 //WHEN I click the save button for that timeblock
 //THEN the text for that event is saved in local storage
 
