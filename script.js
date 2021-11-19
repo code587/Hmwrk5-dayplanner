5079af58afce94de61459c5ed6dd4883765982cd
Stephanie <sandora375@outlook.com>
2021-11-18 23:17:29 -0600

    added textare to input activity

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 //You'll need to use the [Moment.js](https://momentjs.com/) 
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
+
 let today = moment()
 $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
 console.log(today);
 
 
-//WHEN I scroll down
-//THEN I am presented with timeblocks for standard business hours
-
-
-
 //WHEN I view the timeblocks for that day
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
-//WHEN I click into a timeblock
-//THEN I can enter an event
+let hourlyTask = document.querySelector("#activity");
+
+hourlyTask.addEventListener("click", function() {
+  hourlyTask.textContent = ""
+})
+console.log(task);
 
 //WHEN I click the save button for that timeblock
 //THEN the text for that event is saved in local storage
