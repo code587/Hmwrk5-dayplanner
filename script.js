a4382ba8d9f679ce359db9636b75e6f80f982f02
Stephanie <sandora375@outlook.com>
2021-11-30 20:16:04 -0600

    added current time moment for when site is opened

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 
 let task = localStorage.getItem("task");
+let plannerTime = ["Nine","Ten","Eleven","Twelve","One","Two","Three","Four","Five"];
+console.log(plannerTime);
+
+
 
 // current day. Used [Moment.js](https://momentjs.com/) 
 let today = moment()
 $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
 console.log(today);
 
+let currentTime = moment().format("h:mm:a");
+console.log(currentTime);
+
+// let currentHour = ("currentTime");
+// let present = (".present");
+// let pastHour = (".past");
+// let futureHour = (".future");
+
+// function changebgoftime(); 
+//     if (currentHour === currentTime) {
+//         present
+//     }
+//     else if (currentHour < currentTime) {
+//         pastHour
+//     }
+//     else (currentHour > currentTime); {
+//         futureHour
+//     }
+
+
 //WHEN I view the timeblocks for that day
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
 
 //save .description when button clicked 
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
    
-//this is to save description in its sibling and timeBlock of its parent when savebtn clicked
+//this is to save description of its sibling and timeBlock of its parent
   let textAreaValue = $(this).siblings(".description").val();
 console.log(textAreaValue)
   let timeBlock = $(this).parent().attr("id")
